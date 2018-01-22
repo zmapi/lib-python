5a9eba6b58a13ca01b54bc47a33b5a337a79bb08
Sebastian Jylanki <sebastian.jylanki@gmail.com>
2018-01-23 03:11:51 +0700

    Added lru_cache to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 from zmapi.exceptions import *
+from functools import wraps
+from collections import OrderedDict
+import inspect
 
 def check_missing(fields, d):
     if type(fields) is str:
@@ -7,3 +10,24 @@ def check_missing(fields, d):
         if field not in d:
             raise InvalidArgumentsException("missing field: {}"
                                             .format(field))
+
+
+# Modified version of https://gist.github.com/jaredlunde/7a118c03c3e9b925f2bf
+def lru_cache(maxsize=128):
+    cache = OrderedDict()
+    def decorator(fn):
+        @wraps(fn)
+        async def memoizer(*args, **kwargs):
+            key = str((args, kwargs))
+            try:
+                cache[key] = cache.pop(key)
+            except KeyError:
+                if len(cache) >= maxsize:
+                    cache.popitem(last=False)
+                if inspect.iscoroutinefunction(fn):
+                    cache[key] = await fn(*args, **kwargs)
+                else:
+                    cache[key] = fn(*args, **kwargs)
+            return cache[key]
+        return memoizer
+    return decorator
